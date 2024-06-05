53472c1a1bb15d6a4bb424265ecf76c9b41bab40
Lazar Nikolov <hello@nikolovlazar.com>
2024-06-05 12:58:03 -0400

    format

--- src/app/sentry-client-tag.tsx
+++ src/app/sentry-client-tag.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client';
 
-import { getCookie, handleDeviceDetection } from "@/lib/utils";
-import * as Sentry from "@sentry/nextjs";
-import { useEffect, PropsWithChildren } from "react";
+import { getCookie, handleDeviceDetection } from '@/lib/utils';
+import * as Sentry from '@sentry/nextjs';
+import { useEffect, PropsWithChildren } from 'react';
 
 export const SentryClientTag = ({ children }: PropsWithChildren<{}>) => {
   useEffect(() => {
     const dismissedDiscoverOrders =
-      !!getCookie("dismissedDiscoverOrders") ?? false;
+      !!getCookie('dismissedDiscoverOrders') ?? false;
 
-    Sentry.setTag("dismissed-discover-orders", dismissedDiscoverOrders);
+    Sentry.setTag('dismissed-discover-orders', dismissedDiscoverOrders);
 
     const deviceType = handleDeviceDetection();
 
-    Sentry.setTag("device-type", deviceType);
+    Sentry.setTag('device-type', deviceType);
   }, []);
 
   return children;
